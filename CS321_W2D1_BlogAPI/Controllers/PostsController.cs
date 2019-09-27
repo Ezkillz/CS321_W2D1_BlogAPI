e5f370e2026257988dc2998d32ed87d878c876a1
Ezkillz <zzekegomez09@gmail.com>
2019-09-26 20:55:44 -0500

    Commit final product by the end of class today. Yay!

--- CS321_W2D1_BlogAPI/Controllers/PostsController.cs
+++ CS321_W2D1_BlogAPI/Controllers/PostsController.cs
@@ -15,8 +15,9 @@ namespace CS321_W2D1_BlogAPI.Controllers
         // Constructor
         // IPostService is automatically injected by the ASP.NET framework, if you've
         // configured it properly in Startup.ConfigureServices()
-        public PostsController(/* TODO: add a parameter of type IPostService */)
+        public PostsController( IPostService postService)
         {
+            _postService = postService;
             //TODO: keep a reference to the service so we can use it in methods below
         }
 
@@ -25,7 +26,10 @@ namespace CS321_W2D1_BlogAPI.Controllers
         [HttpGet]
         public IActionResult Get()
         {
+
             // TODO: return OK 200 status and list of posts
+            return Ok(_postService.GetAll());
+
         }
 
         // get specific post by id
@@ -35,7 +39,7 @@ namespace CS321_W2D1_BlogAPI.Controllers
         {
             // look up post by id
             // TODO: use _postsService to get post by id
-
+            var post = _postService.Get(Id);
             // if not found, return 404 NotFound 
             if (post == null) return NotFound();
 
@@ -63,8 +67,9 @@ namespace CS321_W2D1_BlogAPI.Controllers
         {
             Post post;
             // TODO: use _postService to update post. store returned Post in the post variable.
+            Post post = _postService.Get(id);
             if (post == null) return NotFound();
-            return Ok(post);
+            return Ok(_postService.Update(updatedPost));
         }
 
         // delete an existing post
@@ -73,9 +78,11 @@ namespace CS321_W2D1_BlogAPI.Controllers
         public IActionResult Delete(int id)
         {
             // TODO: use _postService to get post by id
+            var post = _postService.Get(id);
             if (post == null) return NotFound();
             // TODO: use _postService to update post
             return NoContent();
+            //post is deleted so no need to return anyting 
         }
     }
 }
--- CS321_W2D1_BlogAPI/Services/IPostService.cs
+++ CS321_W2D1_BlogAPI/Services/IPostService.cs
@@ -9,6 +9,7 @@ namespace CS321_W2D1_BlogAPI.Services
         IEnumerable<Post> GetAll();
         Post Get(int id);
         Post Add(Post post);
+        Post update(Post post);
         void Remove(Post post);
     }
 }
